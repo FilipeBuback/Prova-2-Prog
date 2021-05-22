c2b2b93c7a2e3f698ca532314607f1d5a11b2f21
Buback <>
2021-05-22 18:21:07 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  char *num[n];
+  int n;
+  cout << "Digite a quantidade de numeros que quer inserir"<< endl;
+  cin >> n;
+  cout<< "Agora digite os numeros que quer inserir"<<endl;
+ for(int i=0; i<=n;i++){
+   cout<<"Digite:"<<endl;
+
+
+
+ }
+
+
+
+  
+ 
+}
